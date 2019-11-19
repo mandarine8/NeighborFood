92cd90eacef040649cca6c0c01124327c961f2a6
sudo apt update <charlottemontane@hotmail.com>
2019-11-19 11:07:51 -0500

    controller booking added

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,19 @@
 class BookingsController < ApplicationController
+
+    def show
+        @booking = Booking.find(params[:id])
+    end
+
+    def create
+        @booking = Booking.new(booking_params)
+        if @booking.save
+          redirect_to booking_path(@booking)
+        else
+          render "new"
+        end  
+    end
+ private
+    def booking_params
+        params.require(:booking).permit(:user_id, dish_id)
+    end
 end
