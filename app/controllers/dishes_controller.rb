9d895da473baee632fc467a890033a52ebd8d460
Chloe Saurat <sauratchloe@gmail.com>
2019-11-19 15:44:57 -0500

    Views_dishes_index_show_new

--- app/controllers/dishes_controller.rb
+++ app/controllers/dishes_controller.rb
@@ -10,12 +10,14 @@ class DishesController < ApplicationController
 
   def new
     @dish = Dish.new
+    # @user = current_user if current_user
+    @user = User.first
   end
 
   def create
     @dish = Dish.new(dish_params)
-    if @dish.save
-      redirect_to dish_path(@dish)
+    if @dish.save!
+      redirect_to dishes_path(@dish)
     else
       render :new
     end
@@ -26,6 +28,7 @@ class DishesController < ApplicationController
   end
 
   def update
+    @dish = Dish.find(params[:id])
     if @dish.update(dish_params)
       redirect_to dish_path(@dish)
     else
@@ -36,12 +39,12 @@ class DishesController < ApplicationController
   def destroy
     @dish = Dish.find(params[:id])
     @dish.destroy
-    redirect_to dishes_path
+    redirect_to user_path
   end
 
   private
 
   def dish_params
-    params.require(:dish).permit(:name, :description, :price, :photo)
+    params.require(:dish).permit(:name, :description, :price, :photo, :photo_cache, :user_id)
   end
 end
--- app/models/dish.rb
+++ app/models/dish.rb
@@ -1,3 +1,4 @@
 class Dish < ApplicationRecord
   belongs_to :user
+  mount_uploader :photo, PhotoUploader
 end
